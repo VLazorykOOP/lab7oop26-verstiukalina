bfc6451e98a159b3cb49523a7c98e78beb0240b7
IvanSavcynskyi <ivansavchyn2207@gmail.com>
2026-05-20 18:43:17 +0300

    task 2 - template sortuvatyMasyv

--- OOPLab7T/OOPLab7T.cpp
+++ OOPLab7T/OOPLab7T.cpp
@@ -32,3 +32,45 @@ double serednie<char *>(char *arr[], int n)
 
     return (double)sum / n;
 }
+
+template <typename T>
+void sortuvatyMasyv(T arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        T temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+template <>
+void sortuvatyMasyv<char *>(char *arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (strcmp(arr[j], arr[minIndex]) < 0)
+            {
+                minIndex = j;
+            }
+        }
+
+        char *temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
